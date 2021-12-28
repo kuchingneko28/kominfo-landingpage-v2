2835c8f3abda462435327a852d57c7c63f3a57b7
kuchingneko28 <kuchingneko28@gmail.com>
2021-12-28 15:34:35 +0800

    add js

--- index.php
+++ index.php
@@ -179,6 +179,7 @@ $kominfo = getJson($beritaKominfo);
     </footer>
     <!-- Javascript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/scrollreveal"></script>
     <script src="./js/app.js"></script>
   </body>
 </html>
---
+++ js/app.js
@@ -0,0 +1,16 @@
+const sr = ScrollReveal();
+sr.reveal(".hero", {
+  delay: 300,
+  duration: 600,
+  reset: true,
+});
+sr.reveal(".text-awal", {
+  delay: 600,
+  duration: 700,
+  reset: true,
+});
+sr.reveal(".berita-box", {
+  delay: 900,
+  duration: 800,
+  reset: true,
+});
