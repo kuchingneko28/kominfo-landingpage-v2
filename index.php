04c83824734a6bcebe7e466ec1e39e84c336ff4f
kuchingneko28 <kuchingneko28@gmail.com>
2021-12-28 16:17:47 +0800

    g jadi pake efek

--- index.php
+++ index.php
@@ -20,7 +20,6 @@ $kominfo = getJson($beritaKominfo);
     <link rel="shortcut icon" href="https://kominfo.go.id/favicon/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="./css/style.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-    <script src="https://unpkg.com/scrollreveal"></script>
     <title>Kominfo - Wangy Wangy</title>
   </head>
   <body>
