710f96cddd6e097dad4bfadda1cfee6e7615ed6e
kuchingneko28 <kuchingneko28@gmail.com>
2021-12-28 16:15:12 +0800

    salah njirr

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $kominfo = getJson($beritaKominfo);
     <link rel="shortcut icon" href="https://kominfo.go.id/favicon/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="./css/style.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-    <script src="./js/app.js"></script>
+    <script src="https://unpkg.com/scrollreveal"></script>
     <title>Kominfo - Wangy Wangy</title>
   </head>
   <body>
@@ -180,6 +180,6 @@ $kominfo = getJson($beritaKominfo);
     </footer>
     <!-- Javascript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/scrollreveal"></script>
+    <script src="./js/app.js"></script>
   </body>
 </html>
