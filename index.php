c72a023c201bdfcc20c16adecb8a25ab58f60d55
kuchingneko28 <kuchingneko28@gmail.com>
2021-12-31 00:34:09 +0800

    add shapes

--- index.php
+++ index.php
@@ -221,7 +221,7 @@ $kominfo = getJson($beritaKominfo);
               <p>Mentri Kominfo</p>
             </div>
             <div class="profile">
-              <p class="nama">Bapak Johnny Gerard Plate, S.E.</p>
+              <p class="nama shape">Bapak Johnny Gerard Plate, S.E.</p>
               <img class="profile" src="http://kominfo.go.id/images/menteri.jpeg" width="200px" alt="">
             </div>
             </div>  
