655e32697c0bc4337b75b4009232353a8be6f44a
kuchingneko28 <kuchingneko28@gmail.com>
2022-01-10 00:20:03 +0800

    ganti delay effect

--- index.php
+++ index.php
@@ -317,20 +317,20 @@ $siaran = getJson($siaranPers);
     sr.reveal(".hero-title", {
       distance: "-100px",
       origin: "bottom",
-      duration: 800,
-      delay: 500,
+      duration: 1000,
+      delay: 1000,
     });
     sr.reveal(".hero-img", {
       distance: "-100px",
       origin: "bottom",
-      duration: 1000,
-      delay: 500,
+      duration: 2000,
+      delay: 2000,
     });
     sr.reveal(".berita-box", {
       distance: "100px",
       origin: "left",
-      duration: 900,
-      delay: 500,
+      duration: 1000,
+      delay: 1000,
     });
 
     </script>
