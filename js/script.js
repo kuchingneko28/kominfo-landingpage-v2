5bb0aea7d0df12a9d06625355284acb533d497fd
Kuching Neko <73223302+kuchingneko28@users.noreply.github.com>
2022-01-10 07:18:22 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,24 +4,24 @@ sr.reveal(".hero-title", {
   distance: "-100px",
   origin: "bottom",
   duration: 1000,
-  delay: 300,
+  delay: 500,
 });
 sr.reveal(".hero-img", {
   distance: "-100px",
   origin: "bottom",
-  duration: 500,
-  delay: 500,
+  duration: 1000,
+  delay: 750,
 });
 
 sr.reveal(".text-awal", {
   distance: "100px",
   origin: "left",
-  duration: 600,
-  delay: 600,
+  duration: 1000,
+  delay: 900,
 });
 sr.reveal(".berita-box", {
   distance: "100px",
   origin: "left",
-  duration: 700,
-  delay: 700,
+  duration: 1000,
+  delay: 1100,
 });
