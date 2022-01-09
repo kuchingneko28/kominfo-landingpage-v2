05b32ac981b67d976cbcf76bf4a827aee6a9ca3e
kuchingneko28 <kuchingneko28@gmail.com>
2022-01-10 04:17:55 +0800

    ok

--- js/script.js
+++ js/script.js
@@ -3,24 +3,12 @@ const sr = ScrollReveal();
 sr.reveal(".hero-title", {
   distance: "-50px",
   origin: "bottom",
-  duration: 800,
+  duration: 1000,
   delay: 400,
 });
 sr.reveal(".hero-img", {
   distance: "-50px",
   origin: "bottom",
-  duration: 800,
-  delay: 550,
-});
-sr.reveal(" .text-awal", {
-  distance: "50px",
-  origin: "left",
-  duration: 800,
+  duration: 1000,
   delay: 600,
-});
-sr.reveal(".berita-box", {
-  distance: "50px",
-  origin: "left",
-  duration: 800,
-  delay: 700,
 });
