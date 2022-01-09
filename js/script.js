13dddf8839c3ec7001eebdf5d92a8f4f23dcfd15
Kuching Neko <73223302+kuchingneko28@users.noreply.github.com>
2022-01-10 07:24:07 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ sr.reveal(".hero-title", {
   distance: "-100px",
   origin: "bottom",
   duration: 1000,
-  delay: 500,
+  delay: 400,
 });
 sr.reveal(".hero-img", {
   distance: "-100px",
@@ -17,11 +17,11 @@ sr.reveal(".text-awal", {
   distance: "100px",
   origin: "left",
   duration: 1000,
-  delay: 1000,
+  delay: 800,
 });
 sr.reveal(".berita-box", {
   distance: "100px",
   origin: "left",
   duration: 1000,
-  delay: 1100,
+  delay: 900,
 });
