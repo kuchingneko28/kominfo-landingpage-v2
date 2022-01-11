edb98c2168686de834b86d2f4e82b412fec6fbe3
kuchingneko28 <kuchingneko28@gmail.com>
2022-01-11 13:50:09 +0800

    ok

--- js/script.js
+++ js/script.js
@@ -10,18 +10,18 @@ sr.reveal(".hero-img", {
   distance: "-100px",
   origin: "bottom",
   duration: 1000,
-  delay: 700,
+  delay: 500,
 });
 
 sr.reveal(".text-awal", {
   distance: "100px",
   origin: "left",
   duration: 1000,
-  delay: 800,
+  delay: 700,
 });
 sr.reveal(".berita-box", {
   distance: "100px",
   origin: "left",
   duration: 1000,
-  delay: 900,
+  delay: 800,
 });
