0cb5052fa1e674c2d4a985ea2dd5c2440f361725
kuchingneko28 <kuchingneko28@gmail.com>
2022-01-12 23:53:04 +0800

    fix error

--- includes/function.php
+++ includes/function.php
@@ -7,6 +7,7 @@ function getJson($url)
     curl_setopt_array($ch, [
         CURLOPT_URL => $url,
         CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_USERAGENT => "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Mobile Safari/537.36 Edg/97.0.1072.55",
     ]);
 
     $res = curl_exec($ch);
